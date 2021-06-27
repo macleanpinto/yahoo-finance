da2952e9ad8ece2f80379e2a44698ae6749be11e
Maclean <mpinto@twilio.com>
2021-06-27 12:34:50 +0530

    handle error response from yahoo API

--- earning/client.go
+++ earning/client.go
@@ -6,28 +6,19 @@ import (
 	"net/http"
 
 	finance "github.com/piquette/finance-go"
-	"github.com/piquette/finance-go/iter"
 )
 
-// Iter is an iterator for a list of quotes.
-// The embedded Iter carries methods with it;
-// see its documentation for details.
-type Iter struct {
-	*iter.Iter
-}
-
-// Quote returns the most recent Quote
-// visited by a call to Next.
-func (i *Iter) EarningHistory() *[]History {
-	return i.Current().(*[]History)
-}
-
-// Returns Earning History for a symbol.
-
-func GetEarningHistory(symbol string) ([]*History, error) {
+// Earning history for the most recent four quarters.
+func getEpsHistory(symbol string) ([]*History, error) {
 	resp, err := http.Get(finance.YFinURL + "/v10/finance/quoteSummary/" + symbol + "?modules=earningsHistory")
 	if err != nil {
 		err = finance.CreateRemoteError(err)
+		return nil, err
+	}
+
+	if resp.StatusCode >= 400 {
+		err = finance.CreateRemoteErrorS("error response recieved from upstream api")
+		return nil, err
 	}
 
 	body, readErr := ioutil.ReadAll(resp.Body)
@@ -42,6 +33,7 @@ func GetEarningHistory(symbol string) ([]*History, error) {
 	}
 	if result.QuoteSummary.Error != nil {
 		err = finance.CreateRemoteError(result.QuoteSummary.Error)
+		return nil, err
 	}
 	return result.QuoteSummary.Result[0].EarningsHistory.History, err
 }
--- earning/client_test.go
+++ earning/client_test.go
@@ -3,11 +3,18 @@ package earning
 import (
 	"testing"
 
+	finance "github.com/piquette/finance-go"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetEarningHistory(t *testing.T) {
-	response, err := GetEarningHistory("RELIANCE.NS")
+func TestGetEpsHistory(t *testing.T) {
+	response, err := getEpsHistory("RELIANCE.NS")
 	assert.Nil(t, err)
 	assert.NotNil(t, response)
+}
+func TestGetEpsHistoryBadQuote(t *testing.T) {
+	response, err := getEpsHistory("CRAP.NS")
+	assert.NotNil(t, err)
+	assert.Nil(t, response)
+	assert.Equal(t, finance.CreateRemoteErrorS("error response recieved from upstream api"), err)
 }
